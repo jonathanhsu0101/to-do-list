aa3ae9efdad6dfaca3d70b403523fd4e1f705d89
jonathanhsu0101 <jonathanhsu0101@gmail.com>
2019-07-24 16:38:23 -0600

    Finished!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    toDos: [],
+    newThing: { thingToDo: '', done: false }
+  };
+
+  change = event => {
+    this.setState({ newThing: { thingToDo: event.target.value, done: false } });
+  };
+
+  addHandler = () => {
+    let newToDos = [...this.state.toDos];
+    newToDos.push(this.state.newThing);
+    this.setState({
+      toDos: newToDos,
+      newThing: { thingToDo: '', done: false }
+    });
+  };
+
+  deleteHandler = index => {
+    let newToDos = [...this.state.toDos];
+    newToDos.splice(index, 1);
+    this.setState({
+      toDos: newToDos
+    });
+  };
+
+  doneHandler = index => {
+    let newToDos = [...this.state.toDos];
+    newToDos[index].done = true;
+    this.setState({
+      toDos: newToDos
+    });
+  };
+
+  render() {
+    const list = (
+      <div>
+        {this.state.toDos.map((toDo, index) => {
+          if (toDo.done) {
+            return (
+              <div key={index} className="ToDoDone">
+                <button
+                  className="Delete"
+                  onClick={() => this.deleteHandler(index)}
+                >
+                  delete
+                </button>
+                {toDo.thingToDo}
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} className="ToDo">
+                <button
+                  className="Done"
+                  onClick={() => this.doneHandler(index)}
+                >
+                  done
+                </button>
+                <button
+                  className="Delete"
+                  onClick={() => this.deleteHandler(index)}
+                >
+                  delete
+                </button>
+                {toDo.thingToDo}
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+    const inputValue = this.state.newThing.thingToDo;
+    return (
+      <div className="App">
+        <p className="Title">To Do List</p>
+        <p className="Note">Enter a new thing to do</p>
+        <input
+          type="text"
+          className="InputBox"
+          value={inputValue}
+          onChange={this.change}
+        />
+        <button className="Add" onClick={this.addHandler}>
+          Add
+        </button>
+        <div>{list}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
